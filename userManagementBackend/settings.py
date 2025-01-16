bead8287b59b5dd57ed41b9aff45afc2faaf7c51
Nitin Mishra <nitin@amplework.com>
2025-01-16 11:53:38 +0530

    trying to deploy

--- userManagementBackend/settings.py
+++ userManagementBackend/settings.py
@@ -142,7 +142,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# To store static files in a separate directory for production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
